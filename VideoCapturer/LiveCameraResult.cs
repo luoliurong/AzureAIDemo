2910f33cc98c7246a4869cdb597ba8972851368b
AP\LuoLiurR <liurong.luo@cn.unisys.com>
2018-09-03 14:44:44 +0800

    update code

--- VideoCapturer/LiveCameraResult.cs
+++ VideoCapturer/LiveCameraResult.cs
@@ -1,23 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.ProjectOxford.Vision.Contract;
 
 namespace VideoCapturer
 {
-	public class LiveCameraResult
-	{
-		public Microsoft.ProjectOxford.Face.Contract.Face[] Faces { get; set; } = null;
-		public Microsoft.ProjectOxford.Common.Contract.EmotionScores[] EmotionScores { get; set; } = null;
-		public string[] CelebrityNames { get; set; } = null;
-		public Microsoft.ProjectOxford.Vision.Contract.Tag[] Tags { get; set; } = null;
-	}
-
 	public class LiveAnalyzeResult
 	{
 		//for text detection result
-		public Microsoft.ProjectOxford.Vision.Contract.Region[] Regions { get; set; } = null;
+		public Region[] Regions { get; set; } = null;
 
 		//for face identification result
 		public string[] FaceIdentifyResult { get; set; } = null;
